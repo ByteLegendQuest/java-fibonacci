05c2f437385a7242170fe3d1b07d996c005eb347
hah1346798520 <1019746812@qq.com>
2021-12-13 13:39:17 +0000

    Update at 2021-12-13T13:39:17.770769Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -16,6 +16,6 @@ public class Challenge {
         if(n==0||n==1){
             return n;
         }
-        return return fibonacci(n-1)+fibonacci(n-2);
+        return fibonacci(n-1)+fibonacci(n-2);
     }
 }
