620babedb756bc1c05856cc7a24d4cee4a657379
FriedreichBismarck <2360756905@qq.com>
2022-02-24 15:43:07 +0000

    Update at 2022-02-24T15:43:07.715051Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -13,9 +13,9 @@ public class Challenge {
     // ...
     // fibonacci(n) = fibonacci(n - 1) + fibonacci(n - 2)
     public static int fibonacci(int n) {
-           if (n == 1) {
+           if (n == 0) {
             return 0;
-        } else if (n == 2) {
+        } else if (n == 1) {
             return 1;
         } else {
             return fibonacci(n - 1) + fibonacci(n - 2);
