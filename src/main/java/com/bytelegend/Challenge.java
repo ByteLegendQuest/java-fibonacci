8a4c6d730b55ebf06618799e045f36ef1fd6b0da
lemon-altxx <2515782282@qq.com>
2022-03-15 05:35:58 +0000

    Update at 2022-03-15T05:35:58.672096Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -13,6 +13,13 @@ public class Challenge {
     // ...
     // fibonacci(n) = fibonacci(n - 1) + fibonacci(n - 2)
     public static int fibonacci(int n) {
-        return 0;
+        if (0 == n) {
+            return 0;
+        }
+        if (1 == n) {
+            return 1;
+        }
+
+        return fibonacci(n -1) + fibonacci(n-2);
     }
 }
