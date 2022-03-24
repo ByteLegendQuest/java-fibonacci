503a322aafb4545dbc943dad38ab28eaa7460efe
yunghf <yung.hf@outlook.com>
2022-03-24 11:46:47 +0000

    Update at 2022-03-24T11:46:47.185718Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -18,6 +18,6 @@ public class Challenge {
         if ( n == 2 )
             { return 1; }
         
-        return fibonacci( n-1 ) + fibonacci( n-2 );
+        return fibonacci( n - 1 ) + fibonacci( n - 2 );
     }
 }
