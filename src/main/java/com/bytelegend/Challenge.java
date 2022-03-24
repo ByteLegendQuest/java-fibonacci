787f74da3ed936d65ca8236c44e188f73d5fc47a
yunghf <yung.hf@outlook.com>
2022-03-24 12:07:09 +0000

    Update at 2022-03-24T12:07:09.176982Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -13,10 +13,10 @@ public class Challenge {
     // ...
     // fibonacci(n) = fibonacci(n - 1) + fibonacci(n - 2)
     public static int fibonacci(int n) {
-        if (n == 1){
+        if (n == 0) {
             return 0;
         }
-        if (n == 2){
+        if (n == 1 || n == 2) {
             return 1;
         }
         return fibonacci(n - 1) + fibonacci(n - 2);
