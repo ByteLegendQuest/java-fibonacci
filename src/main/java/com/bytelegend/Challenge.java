bd864b915b3dcb89ded3b1c37022c016298d183d
SN-Beaux <Beaux.Luo@supernovacompanies.biz>
2022-03-31 05:49:11 +0000

    Update at 2022-03-31T05:49:11.618731Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -13,6 +13,12 @@ public class Challenge {
     // ...
     // fibonacci(n) = fibonacci(n - 1) + fibonacci(n - 2)
     public static int fibonacci(int n) {
-        return 0;
+        if (n == 0) {
+            return n;
+        }
+        if (n <= 2) {
+            return 1;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
