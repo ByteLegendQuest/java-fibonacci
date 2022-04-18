a68bc488eb01b19595266814a8450e2a6c918ff3
wu2171613 <wu2171613@gmail.com>
2022-04-18 14:07:15 +0000

    Update at 2022-04-18T14:07:15.214250Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -20,9 +20,9 @@ public class Challenge {
             a[0] = 0;
             a[1] = 1;
             for (int i = 2; i < n; i++) {
-                a[i] = a[i-1] + a[i-2];
+                a[i] = a[i - 1] + a[i - 2];
             }
-            return a[n-1];
+            return a[n - 1];
         }
     }
 }
