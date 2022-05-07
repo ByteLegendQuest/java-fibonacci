db8eab5f2efd3a234b08d4630dbf6c474a9535c5
kangming06 <kangming06@hotmail.com>
2022-05-07 07:42:12 +0000

    Update at 2022-05-07T07:42:12.320971Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -16,8 +16,8 @@ public class Challenge {
         if (n == 0) {
             return 0;
         } else if (n==1) {
-            return 1
+            return 1;
         } else
-        return fibonacci(n - 1)+fibonacci(n - 2)
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
