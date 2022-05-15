69134082a3bd6f3d15bc868e832e1095224b465b
baixiangyang <443771196@qq.com>
2022-05-15 08:42:47 +0000

    Update at 2022-05-15T08:42:47.292502Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,13 +12,12 @@ public class Challenge {
     // fibonacci(3) = 2
     // ...
     // fibonacci(n) = fibonacci(n - 1) + fibonacci(n - 2)
-    public static int fibonacci(int n) {
-        if(n <= 2)
-        {
-            return =1;
-        }else {
-             return fibonacci(n-1)+fibonacci(n-2);
+     public static int fibonacci(int n) {
+        if (n <= 2) {
+            return 1;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
         }
-       
+
     }
 }
