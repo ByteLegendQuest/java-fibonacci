943b5330cbe4a0b6bcaed99cc689210a873bd5dd
Birlinha <omenaribeiro13@gmail.com>
2022-07-11 04:11:19 +0000

    Update at 2022-07-11T04:11:19.544386Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -13,10 +13,11 @@ public class Challenge {
     // ...
     // fibonacci(n) = fibonacci(n - 1) + fibonacci(n - 2)
     public static int fibonacci(int n) {
-        if((n == 0) || (n == 1))
+        if((n == 0) || (n == 1)) {
             return n;
-        else
+        }
+        else {
             return fibonacci(n - 1) + fibonacci(n - 2);
-        return 0;
+        }
     }
 }
