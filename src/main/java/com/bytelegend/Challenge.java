027d9de4f232b39e6cc7c06822fb3b6a6a247b16
DescentDD <1740811552@qq.com>
2022-07-15 07:33:08 +0000

    Update at 2022-07-15T07:33:08.457666Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -13,6 +13,9 @@ public class Challenge {
     // ...
     // fibonacci(n) = fibonacci(n - 1) + fibonacci(n - 2)
     public static int fibonacci(int n) {
-        return 0;
+        if (n==0)||(n==1)
+        return n;
+        else
+        return fibonacci(n-1)+fibonacci(n-2);
     }
 }
