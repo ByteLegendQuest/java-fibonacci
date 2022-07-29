0e4be330cec8a1266bb7f81484cbd9adf03ee3c3
SaraadKun <35584589+SaraadKun@users.noreply.github.com>
2022-07-29 09:57:26 +0000

    Update at 2022-07-29T09:57:26.198572Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,6 +1,9 @@
 package com.bytelegend;
 
 public class Challenge {
+
+    private static int[] num = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144};
+
     public static void main(String[] args) {
         System.out.println(fibonacci(1));
         System.out.println(fibonacci(1));
@@ -13,6 +16,6 @@ public class Challenge {
     // ...
     // fibonacci(n) = fibonacci(n - 1) + fibonacci(n - 2)
     public static int fibonacci(int n) {
-        return 0;
+        return num[n];
     }
 }
