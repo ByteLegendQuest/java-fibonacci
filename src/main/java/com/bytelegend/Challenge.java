e6fa0c5eb695f0bc2cf8788a38a297800f442245
Umptor <bongutcha@gmail.com>
2022-08-14 08:50:18 +0000

    Update at 2022-08-14T08:50:18.366993Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,6 +1,7 @@
 package com.bytelegend;
 
 public class Challenge {
+
     public static void main(String[] args) {
         System.out.println(fibonacci(1));
         System.out.println(fibonacci(1));
@@ -12,7 +13,28 @@ public class Challenge {
     // fibonacci(3) = 2
     // ...
     // fibonacci(n) = fibonacci(n - 1) + fibonacci(n - 2)
-    public static int fibonacci(int n) {
-        return 0;
-    }
+	private final static Map<Integer, Integer> fiboCache = new HashMap<>();
+
+	public static int fibonacci(int n) {
+		if(n == 0 || n == 1) return n;
+
+		int left;
+		int right;
+		
+		if(fiboCache.containsKey(n - 1)) {
+			right = fiboCache.get(n - 1);
+		} else {
+			right = fibonacci(n - 1);
+			fiboCache.put(n - 1, right);
+		}
+		
+		if(fiboCache.containsKey(n - 2)) {
+			left = fiboCache.get(n - 2);
+		} else {
+			left = fibonacci(n - 2);
+			fiboCache.put(n - 2, left);
+		}
+		
+		return left + right;
+	}
 }
