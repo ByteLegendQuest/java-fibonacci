2428ecacfcf0615e6187bfb477dca0e239974e10
NotAWeeaboo <billyyoo052@gmail.com>
2022-08-15 08:36:06 +0000

    Update at 2022-08-15T08:36:06.285443Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -13,6 +13,8 @@ public class Challenge {
     // ...
     // fibonacci(n) = fibonacci(n - 1) + fibonacci(n - 2)
     public static int fibonacci(int n) {
-        return 0;
+        if(){
+        return (n - 1 + n - 2);
+
     }
 }
