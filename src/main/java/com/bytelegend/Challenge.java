4db96fe9387531d45b35f62b0f2acf03a76998f9
cdwc233 <2241830325@outlook.com>
2022-08-31 05:55:52 +0000

    Update at 2022-08-31T05:55:52.244266Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -13,9 +13,8 @@ public class Challenge {
     // ...
     // fibonacci(n) = fibonacci(n - 1) + fibonacci(n - 2)
     public static int fibonacci(int n) {
-        if (n < 3) {
-            return 1;
-        }
+        if (n == 0) return 0;
+        else if (n < 3) return 1;
         else return  fibonacci(n - 1)+fibonacci(n - 2);
     }
 }
