f8cef698a68979c883fd9d943bb7df15a5afc1b4
Zeon3 <1366685013@qq.com>
2022-09-27 14:59:19 +0000

    Update at 2022-09-27T14:59:19.517649Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -13,7 +13,7 @@ public class Challenge {
     // ...
     // fibonacci(n) = fibonacci(n - 1) + fibonacci(n - 2)
     public static int fibonacci(int n) {
-        if (n > 2 && fibonacci(n) == fibonacci(n - 1) + fibonacci(n - 2)) {
+        if (fibonacci(n) == fibonacci(n - 1) + fibonacci(n - 2)) {
             return n;
         }
         else if (n == 1) {
