99a505121eb074d0967ca0cb8a586ce000bb73e7
Newelle <wozi1988@proton.me>
2022-11-12 13:40:25 +0000

    Update at 2022-11-12T13:40:25.878884Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -13,6 +13,7 @@ public class Challenge {
     // ...
     // fibonacci(n) = fibonacci(n - 1) + fibonacci(n - 2)
     public static int fibonacci(int n) {
-        return fibonacci(n-1) + fibonacci(n-2);
+
+        return n > 0 ? fibonacci(n-1) + fibonacci(n-2) : 0;
     }
 }
