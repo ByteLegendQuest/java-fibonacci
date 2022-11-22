f271a17bd5c39acb40ee7380938af5f9e41cdeb1
Lin-kq <2628431588@qq.com>
2022-11-22 10:25:13 +0000

    Update at 2022-11-22T10:25:13.168576Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -13,6 +13,6 @@ public class Challenge {
     // ...
     // fibonacci(n) = fibonacci(n - 1) + fibonacci(n - 2)
     public static int fibonacci(int n) {
-        return (n <= 2 ? 1 : fibonacci(n - 1) + fibonacci(n - 2));
+        return (n <= 2 ? (n == 0 ? 0 : 1) : fibonacci(n - 1) + fibonacci(n - 2));
     }
 }
