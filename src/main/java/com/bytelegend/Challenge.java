08eabc73e5a23116069d9c071871edd07e5f01fc
itqihou <dongzhoudynasty@outlook.com>
2022-11-24 00:52:40 +0000

    Update at 2022-11-24T00:52:40.221831Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -17,7 +17,7 @@ public class Challenge {
             return -1;
         if n == 0:
             return 0;
-        if n == 1:
+        if n <= 2:
             return 1;
         return fibonacci(n-1)+fibonacci(n-2);
     }
