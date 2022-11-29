5550b3ec7d818919f2571eaaacfac0c0b27a2466
fakenoob20222 <15260160218@163.com>
2022-11-29 04:33:30 +0000

    Update at 2022-11-29T04:33:30.254782Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,11 +12,12 @@ public class Challenge {
     // fibonacci(3) = 2
     // ...
     // fibonacci(n) = fibonacci(n - 1) + fibonacci(n - 2)
-    public static int fibonacci(int n) {
-               if(n < 2){
-            return n;
-        }else {
-            return  n + fibonacci(n - 1);
-        }
+ public static int fibonacci(int n) {
+     if(n < 2){
+        return  n;
+     }else{
+         return  fibonacci(n - 1) + fibonacci( n - 2);
+     }
+
     }
 }
