cb692c604b3006d92c361847244a02e031aa8bfd
CITVAIM <citvaim@icloud.com>
2023-01-23 16:33:06 +0000

    Update at 2023-01-23T16:33:06.350490Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -3,7 +3,9 @@ package com.bytelegend;
 public class Challenge {
     public static void main(String[] args) {
         System.out.println(fibonacci(1));
-        System.out.println(fibonacci(1));
+        System.out.println(fibonacci(2));
+        System.out.println(fibonacci(3));
+        System.out.println(fibonacci(4));
     }
 
     // fibonacci(0) = 0
@@ -13,6 +15,13 @@ public class Challenge {
     // ...
     // fibonacci(n) = fibonacci(n - 1) + fibonacci(n - 2)
     public static int fibonacci(int n) {
-        return 0;
+        if(n == 0) {
+            return 0;
+        } else if (n == 1 || n == 2) {
+            return 1;
+        } else {
+            return fibonacci(n-1) + fibonacci(n-2);
+        }
+
     }
 }
