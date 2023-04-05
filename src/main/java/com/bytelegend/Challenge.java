d7728f057fc44703185c27b0cf9cfb63dea0db93
123u414 <1652888156@qq.com>
2023-04-05 03:37:17 +0000

    Update at 2023-04-05T03:37:17.001162Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -16,6 +16,9 @@ public class Challenge {
         int[] temp = new int[3];
         temp[1] = 1;
         temp[2] = 1;
+        if (n <= 0) {
+            return 0;
+        }
         if (n == 1 || n == 2) {
             return 1;
         } else {
