a25e51e3ed8f8fd49f9c58b9a4b71b47ae95f619
123u414 <1652888156@qq.com>
2023-04-05 03:34:34 +0000

    Update at 2023-04-05T03:34:33.989918Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -13,6 +13,18 @@ public class Challenge {
     // ...
     // fibonacci(n) = fibonacci(n - 1) + fibonacci(n - 2)
     public static int fibonacci(int n) {
-        return 0;
+        int[] temp = new int[3];
+        temp[1] = 1;
+        temp[2] = 1;
+        if (n == 1 || n == 2) {
+            return 1;
+        } else {
+            for (int i = 2; i < n; i++) {
+                temp[0] = temp[1];
+                temp[1] = temp[2];
+                temp[2] = temp[0] + temp[1];
+            }
+            return temp[2];
+        }
     }
 }
