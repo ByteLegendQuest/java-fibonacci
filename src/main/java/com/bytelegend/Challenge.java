f04f34096e92637974a432d4904d803c48f8bb76
StayAlivez <1214117288@qq.com>
2023-05-19 08:38:57 +0000

    Update at 2023-05-19T08:38:57.249100Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,25 +1,41 @@
 package com.bytelegend;
 
 public class Challenge {
+    // 创建一个数组来存储已经计算过的结果
+    private static int[] memo;
+
     public static void main(String[] args) {
         System.out.println(fibonacci(1));
-        System.out.println(fibonacci(1));
+        System.out.println(fibonacci(2));
+        System.out.println(fibonacci(5));
+        System.out.println(fibonacci(10));
+        System.out.println(fibonacci(20));
     }
 
-    // fibonacci(0) = 0
-    // fibonacci(1) = 1
-    // fibonacci(2) = 1
-    // fibonacci(3) = 2
-    // ...
-    // fibonacci(n) = fibonacci(n - 1) + fibonacci(n - 2)
     public static int fibonacci(int n) {
+        // 初始化记忆数组
+        memo = new int[n + 1];
+        return fibonacciHelper(n);
+    }
+
+    private static int fibonacciHelper(int n) {
+        // 如果已经计算过，直接返回存储的结果
+        if (memo[n] != 0) {
+            return memo[n];
+        }
+
+        // 计算并存储结果
+        int result;
         if (n == 1) {
-            return 0;
+            result = 0;
+        } else if (n == 2) {
+            result = 1;
+        } else {
+            result = fibonacciHelper(n - 1) + fibonacciHelper(n - 2);
         }
-        if (n == 2) {
-            return 1;
-        }
-        return fibonacci(n - 1) + fibonacci(n - 2);
+        memo[n] = result;
+
+        return result;
     }
 }
 
