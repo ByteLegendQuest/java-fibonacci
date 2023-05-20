2c581fe17f3ac57da58ecbb6b964d1a0264a6ca6
danthe1st <daniel@wwwmaster.at>
2023-05-20 15:07:00 +0000

    Update at 2023-05-20T15:07:00.261947Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -16,6 +16,9 @@ public class Challenge {
         if (n <= 1) {
             return n;
         }
+        if(n == 2) {
+            return 1;
+        }
         return fibonacci(n - 1) + fibonacci(n - 2)
     }
 }
