21b763345534aa901cb5a20e0837ecc7bec3e66a
NahuelBullonJensen <nahuelbullonjensen@gmail.com>
2023-05-21 05:41:56 +0000

    Update at 2023-05-21T05:41:56.977327Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,7 +12,12 @@ public class Challenge {
     // fibonacci(3) = 2
     // ...
     // fibonacci(n) = fibonacci(n - 1) + fibonacci(n - 2)
-    public static int fibonacci(int n) {
-        return 0;
+    public int fibonacci(int n) {
+        if (n <= 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
