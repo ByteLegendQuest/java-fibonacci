8d7785dde6a10b510379409ac99159bcc2ea9caf
rosettastoned12 <officiallynabiel@gmail.com>
2023-05-24 03:12:43 +0000

    Update at 2023-05-24T03:12:43.769129Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -21,8 +21,8 @@ package com.bytelegend;
 
 public class Challenge {
     public static void main(String[] args) {
-        fibonacci(1);
-        fibonacci(8);
+        System.out.println(fibonacci(1));
+        System.out.println(fibonacci(1));
     }
 
     // fibonacci(0) = 0
@@ -31,19 +31,14 @@ public class Challenge {
     // fibonacci(3) = 2
     // ...
     // fibonacci(n) = fibonacci(n - 1) + fibonacci(n - 2)
-    public static void fibonacci(int n) {
+    public static int fibonacci(int n) {
         int a = 0,b = 1,c;
-        if (n == 1 || n == 0)
-            System.out.println(1);
+        if (n == 0)
+            return 0;
+        else if (n == 1)
+            return 1;
         else {
-            System.out.println(1);
-            System.out.println(b);
-            for (int i = 0; i < n; i++) {
-                c = a + b;
-                System.out.println(c);
-                a = b;
-                b = c;
-            }
+            return fibonacci(n-1)+fibonacci(n-2);
         }
     }
 }
